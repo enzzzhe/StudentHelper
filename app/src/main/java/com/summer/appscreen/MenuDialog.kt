f4322ece7f0ebde03f964b432b6b5c21d68807f7
Daria Turpetkina <daria.tur.ddt@gmail.com>
2022-07-11 14:31:58 +0300

    Changes Added

--- app/src/main/java/com/summer/appscreen/MenuDialog.kt
+++ app/src/main/java/com/summer/appscreen/MenuDialog.kt
@@ -3,19 +3,23 @@ package com.summer.appscreen
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.fragment.app.DialogFragment
 
 class MenuDialog: DialogFragment() {
+
     private val actions = arrayOf("Редактировать","Удалить","Отмена")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
             builder.setItems(actions){
                 //dialog, which -> Toast.makeText(activity, "${actions[which]}", Toast.LENGTH_SHORT).show()
                 dialog, item -> if (actions[item] == "Редактировать"){
-
+                    builder.setView(inflater.inflate(R.layout.prompt, null))
+                    builder.create()
                 }else if (actions[item] == "Удалить"){
 
                 }else if (actions[item] == "Отмена"){
@@ -24,5 +28,6 @@ class MenuDialog: DialogFragment() {
             }
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
+        val text = arguments?.putString("name", "r")
     }
 }
