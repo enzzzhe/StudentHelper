828f00d4e5881ec9d72596cb1d50ec2c9b72ef16
Polina Mikheeva <mikheeva_polli@mail.ru>
2022-07-11 17:45:30 +0300

    main screen and navigation

---
+++ app/src/main/java/com/example/summerpractice/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.example.summerpractice
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.summerpractice.databinding.FragmentMainBinding
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
